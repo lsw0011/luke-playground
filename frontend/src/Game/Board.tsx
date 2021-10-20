2112f56a50eba64efe9351f83ccdbb5bb58851c3
Luke Westerfield <lsw@protobrand.com>
2021-10-20 14:13:12 +0200

    Logic completed

--- frontend/src/Game/Board.tsx
+++ frontend/src/Game/Board.tsx
@@ -5,17 +5,160 @@ import Square from './Square'
 
 const Board = () => {
     
-    React.useEffect(() => {
-        const square = { value: 0, merged: false}
-        const location = {filled: false, square: square}
-        const grid = new Array(4).map(() => {
-            const loc = { value: 0, square: Object.assign({}, square) }
-            return Object.assign({}, location)
+    const printBoard = (arr) => {
+        arr.reduce((prev, curr, index) => {
+            const next = prev + ' ' + curr
+            if(index % 4 === 3) {
+                if(index === 3) console.log(' ' + next);
+                else console.log(next);
+                return('')
+            }
+            return(next)
         })
-
-        grid[1].square.value = 2
-        console.log(grid)
-    },[])
+    }
+    
+    const generateRandomSquare = (array) => {
+        const openIndexes = [];
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] === 0) openIndexes.push(i);
+        }
+        if (openIndexes.length === 0) return false;
+        const newLocation = Math.floor(Math.random() * openIndexes.length);
+        array[openIndexes[newLocation]] = 2
+        return true;
+    }
+    
+    
+    const shiftValue = (array, i1, i2) => {
+        array[i1] = array[i2];
+        array[i2] = 0;
+    }
+    
+    const doubleValue = (array, i1, i2) => {
+        array[i1] *= 2;
+        array[i2] = 0;
+    }
+    
+    const getRow = (array, row) => {
+        const nRow = []
+        for(let i = row * 4; i < row * 4 + 4; i++) {
+            nRow.push(array[i])
+        }
+        return nRow;
+    }
+    
+    const insertRow = (array, nRow, row) => {
+        for(let i = 0; i < 4; i++) {
+            array[row * 4 + i] = nRow[i];
+        } 
+    }
+    
+    const getColumn = (array, column) => {
+        const nColumn = [];
+        for(let i = column; i / 4 < 4; i += 4) {
+            nColumn.push(array[i])
+        }
+        return nColumn;
+    }
+    
+    const insertColumn = (array, nColumn, column) => {
+        for(let i = 0; i < 4; i ++) {
+            array[i * 4 + column] = nColumn[i]
+        }
+    }
+    
+    const shiftZeros = (row, start, stop, dir) => {
+        if(start === stop) return;
+        if(row[start] === 0) {
+            shiftZeros(row, start - dir, stop, dir);
+            shiftValue(row, start, start - dir);
+        }
+        shiftZeros(row, start - dir, stop, dir);
+    }
+    
+    
+    const shiftForward = (row, end) => {
+        for(let i = end - 1; i > 0; i--) {
+            shiftZeros(row, i, 0, 1);
+            if(row[i - 1] === row[i]) {
+                doubleValue(row, i, i - 1);
+                shiftZeros(row, i, 0, 1);
+                shiftForward(row, i - 1);
+            }
+        }
+    }
+    
+    const shiftBackward = (row, start) => {
+        for(let i = start; i < row.length - 1; i++) {
+            shiftZeros(row, i, row.length - 1, -1)
+            if(row[i + 1] === row[i]) {
+                doubleValue(row, i, i + 1);
+                shiftZeros(row, i, row.length - 1, -1)
+                shiftBackward(row, i + 1);
+            }
+        }
+    }
+            
+    const right = (arr) => {
+        for(let i = 0; i < 4; i++) {
+            const row = getRow(arr, i);
+            shiftForward(row, row.length);
+            insertRow(arr, row, i);
+        }
+    }
+    
+    const down = (arr) => {
+        for(let i = 0; i < 4; i++) {
+            const column = getColumn(arr, i);
+            shiftForward(column, column.length);
+            insertColumn(arr, column, i);
+        }
+    }
+    
+    const left = (arr) => {
+        for(let i = 0; i < 4; i++) {
+            const row = getRow(arr, i);
+            shiftBackward(row, 0);
+            insertRow(arr, row, i);
+        }
+    }
+    
+    const up = (arr) => {
+        for(let i = 0; i < 4; i++) {
+            const column = getColumn(arr, i);
+            shiftBackward(column, 0);
+            insertColumn(arr, column, i);
+        }
+    }
+    
+    const movement = (arr, handler) => {
+        const arrClone = Object.assign([], arr);
+        handler(arr)
+        for(let i = 0; i < 16; i++) {
+            if(arr[i] !== arrClone[i]) return true;
+        }
+        return false
+    }
+    const [gameArray, setGameArray] = React.useState()
+    const gameArray = new Array(16).fill(0)
+    generateRandomSquare(gameArray)
+    generateRandomSquare(gameArray)
+    window.addEventListener('keydown', (e) => {
+        let change;
+        if(e.code === 'ArrowUp') {
+            change = movement(gameArray, up)
+        } else if (e.code === 'ArrowDown') {
+            change = movement(gameArray, down)
+        } else if (e.code === 'ArrowLeft') {
+            change = movement(gameArray, left)
+    	} else if(e.code === 'ArrowRight') {
+            change = movement(gameArray, right)
+	    }
+        if(change){ 
+            generateRandomSquare(gameArray);
+            printBoard(gameArray);
+        }
+    })
 
     return (
         <>
